78bbd457c5300d92af231dc898f0f881b4c17d7b
a-brown77 <a.brown7795@gmail.com>
2022-03-31 22:24:50 -0400

    started login and signup

---
+++ smash-community-app/src/components/SignUp.tsx
@@ -0,0 +1,42 @@
+import { useState } from "react";
+
+export function SignUp() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
+  function handleSubmit(e: any) {
+    e.preventDefault();
+  }
+
+  return (
+    <div className="SignUpContainer">
+      <form>
+        <p>Sign Up</p>
+        <label>
+          <p>Email:</p>
+          <input type="text" onChange={(e) => setEmail(e.target.value)} />
+        </label>
+        <label>
+          <p>Password</p>
+          <input
+            type="password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </label>
+        <label>
+          <p>Confirm Password</p>
+          <input
+            type="password"
+            onChange={(e) => setConfirmPassword(e.target.value)}
+          />
+        </label>
+        <div>
+          <button className="button" type="submit" onClick={handleSubmit}>
+            Submit
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+}
--- smash-community-app/src/index.tsx
+++ smash-community-app/src/index.tsx
@@ -1,14 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import "./index.css";
+import App from "./App";
+import ReactDOM from "react-dom";
+import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Landing } from "./components/Landing";
+import * as ReactDOMClient from "react-dom/client";
 
-ReactDOM.render(
+const container = document.getElementById("app");
+
+const root = ReactDOMClient.createRoot(container);
+
+root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <BrowserRouter>
+      <App />
+      <Routes>
+        <Route path="/" element={<Landing />} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
