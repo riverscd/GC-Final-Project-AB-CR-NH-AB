a299706de74364d034ef951b79429cd5acc4c466
a-brown77 <a.brown7795@gmail.com>
2022-03-31 22:58:24 -0400

    new front-end file

---
+++ smash-frontend/src/components/Login.tsx
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+// import { Link, useNavigate } from "react-router-dom";
+
+export function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  function handleSubmit(e: any) {
+    e.preventDefault();
+  }
+
+  return (
+    <div className="loginContainer">
+      <form>
+        <p>Welcome Back to the Smash Melee Community</p>
+        <label>
+          <p>Email:</p>
+          <input type="text" onChange={(e) => setEmail(e.target.value)} />
+        </label>
+        <label>
+          <p>Password:</p>
+          <input type="text" onChange={(e) => setPassword(e.target.value)} />
+        </label>
+      </form>
+
+      <div>
+        <button className="button" type="submit" onClick={handleSubmit}>
+          Submit
+        </button>
+      </div>
+    </div>
+  );
+}
