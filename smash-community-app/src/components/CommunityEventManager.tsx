c0e0dd6e0c48ce37cddab3f36703250fc0dcc2b1
a-brown77 <a.brown7795@gmail.com>
2022-04-02 20:33:53 -0400

    added buttons to event manager

--- smash-community-app/src/components/CommunityEventManager.tsx
+++ smash-community-app/src/components/CommunityEventManager.tsx
@@ -1,9 +1,25 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 export function CommunityEventManager() {
+  const navigate = useNavigate();
+
+  function handleCreateCommunity() {
+    navigate("/CommunityForm");
+  }
+
+  function handleCreateEvent() {
+    navigate("/EventForm");
+  }
+
   return (
     <div>
       <h1>Community and Event Manager</h1>
+      <button className="button" type="submit" onClick={handleCreateCommunity}>
+        Create a Community
+      </button>
+      <button className="button" type="submit" onClick={handleCreateEvent}>
+        Create an Event
+      </button>
       <Link to="/">Home</Link>
     </div>
   );
