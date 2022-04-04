2d377e7462ee7f3ff4a7cd88457d7a23653df7d9
a-brown77 <a.brown7795@gmail.com>
2022-04-03 21:36:24 -0400

    updated sign up

--- smash-community-app/src/components/SignUp.tsx
+++ smash-community-app/src/components/SignUp.tsx
@@ -3,6 +3,9 @@ import { Link, useNavigate } from "react-router-dom";
 
 export function SignUp() {
   const [email, setEmail] = useState("");
+  const [username, setUsername] = useState("");
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
   const navigate = useNavigate();
@@ -19,6 +22,18 @@ export function SignUp() {
         <label>
           <p>Email:</p>
           <input type="text" onChange={(e) => setEmail(e.target.value)} />
+        </label>
+        <label>
+          <p>Username:</p>
+          <input type="text" onChange={(e) => setUsername(e.target.value)} />
+        </label>
+        <label>
+          <p>First Name:</p>
+          <input type="text" onChange={(e) => setFirstName(e.target.value)} />
+        </label>
+        <label>
+          <p>Last Name:</p>
+          <input type="text" onChange={(e) => setLastName(e.target.value)} />
         </label>
         <label>
           <p>Password</p>
---
+++ smash-community-app/src/services/smash-api.ts
@@ -0,0 +1,7 @@
+import axios from "axios";
+
+// export function getAllTournaments() {
+//   return axios.get("/tournaments").then(function (response: any) {
+//     return response.data;
+//   });
+// }
