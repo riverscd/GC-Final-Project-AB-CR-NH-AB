2c99cd0f7b0b3cc027188efb29c5f532b998be89
a-brown77 <a.brown7795@gmail.com>
2022-04-05 22:34:11 -0400

    idek why it wont let me pull

--- smash-backend/index.ts
+++ smash-backend/index.ts
@@ -1,9 +1,8 @@
-import express from 'express';
-import cors from 'cors';
-import pg from 'pg-promise';
-import tournamentroutes from './routes/tournamentroutes';
-import 'dotenv/config';
-
+import express from "express";
+import cors from "cors";
+import pg from "pg-promise";
+import tournamentroutes from "./routes/tournamentroutes";
+import "dotenv/config";
 
 const app = express();
 
@@ -13,17 +12,15 @@ app.use(cors());
 
 app.use(express.json());
 
-
 // export const db = pg()({
 // 	host:  process.env.DB_HOST,
 // 	port:  parseInt(process.env.DB_PORT),
 // 	user:  process.env.DB_USER,
 // 	password: process.env.DB_PASS,
 // 	database: process.env.DB_DATABASE,
-	
+
 // });
 
-app.use('/', tournamentroutes);
+app.use("/", tournamentroutes);
 
-
-app.listen(port, () => console.log(`Server is listening on port ${port}.`))
+app.listen(port, () => console.log(`Server is listening on port ${port}.`));
