7a5ee9ad39347e107fb90c4d123599b9392f8b16
Nick <nhamer233@gmail.com>
2022-04-07 21:22:46 -0400

    Added signup and login routes to backend

---
+++ smash-backend/Models/users.ts
@@ -0,0 +1,9 @@
+export interface Users {
+  user: User[];
+}
+
+export interface User {
+  id: number;
+  email: string;
+  password: string;
+}
--- smash-backend/routes/userRoutes.ts
+++ smash-backend/routes/userRoutes.ts
@@ -1,82 +1,115 @@
 import express from "express";
-import fetch from "node-fetch";
-import pg from "pg-promise";
-
 import { db } from "../index";
+import bcrypt from "bcrypt";
+import { Body } from "node-fetch";
+import { User } from "../Models/users";
+
+const saltRounds = 10;
 
 const userRoutes = express.Router();
 
 userRoutes.get("/users", (req, res) => {
-  db.manyOrNone("select * from users")
+  db.manyOrNone("SELECT * FROM users")
     .then((data) => res.json(data))
     .catch((error) => console.log(error));
 });
 userRoutes.get("/users/:id", (req, res) => {
-  db.oneOrNone("select * from users where id = $(id)", { id: req.params.id })
+  db.oneOrNone("SELECT * FROM users WHERE id = $(id)", { id: req.params.id })
     .then((data) => res.json(data))
     .catch((error) => console.log(error));
 });
 
-userRoutes.post("/users", (req: any, res: any) => {
-  const newUser = {
-    username: req.body.username,
-    password: req.body.password,
-    first_name: req.body.first_name,
-    last_name: req.body.last_name,
-    email: req.body.email,
-    birthdate: req.body.birthdate,
-    city: req.body.city,
-    state: req.body.state,
-    country: req.body.country,
-    zip: req.body.zip,
-    user_profile_img: req.body.user_profile_img,
-    bio: req.body.bio,
-    main_character: req.body.main_character,
-    secondary_character: req.body.secondary_character,
-    slippi_usernames: req.body.slippi_usernames,
-  };
-  // const valid = schema.validate(newUser);
+// userRoutes.post("/users", (req: any, res: any) => {
+//   const newUser = {
+//     username: req.body.username,
+//     password: req.body.password,
+//     first_name: req.body.first_name,
+//     last_name: req.body.last_name,
+//     email: req.body.email,
+//     birthdate: req.body.birthdate,
+//     city: req.body.city,
+//     state: req.body.state,
+//     country: req.body.country,
+//     zip: req.body.zip,
+//     user_profile_img: req.body.user_profile_img,
+//     bio: req.body.bio,
+//     main_character: req.body.main_character,
+//     secondary_character: req.body.secondary_character,
+//     slippi_usernames: req.body.slippi_usernames,
+//   };
+//   // const valid = schema.validate(newUser);
 
-  // if(valid.error) {
-  //    return res.status(400).send(valid.error)
-  // }
+//   // if(valid.error) {
+//   //    return res.status(400).send(valid.error)
+//   // }
 
-  db.oneOrNone(
-    "insert into user (username, password, first_name, last_name, email, birthdate, city, state, country, zip, user_profile_img, bio, main_character, secondary_character, slippi_usernames) values ($(username), $(password), $(first_name), $(last_name), $(email), $(birthdate), $(city), $(state), $(country), $(zip), $(user_profile_img), $(bio), $(main_character), $(secondary_character), $(slippi_usernames) returning id",
-    newUser
-  )
+//   db.oneOrNone(
+//     "INSER INTO users (username, password, first_name, last_name, email, birthdate, city, state, country, zip, user_profile_img, bio, main_character, secondary_character, slippi_usernames) values \
+//      ($(username), $(password), $(first_name), $(last_name), $(email), $(birthdate), $(city), $(state), $(country), $(zip), $(user_profile_img),\
+//        $(bio), $(main_character), $(secondary_character), $(slippi_usernames) returning id",
+//     newUser
+//   )
 
-    .then((id) => {
-      db.one("select * from users where id = $(id)", { id: id.id }).then(
-        (todo) => res.json(todo)
-      );
-    })
-    .catch((error) => res.status(500).send(error));
-});
+//     .then((id) => {
+//       db.one("SELECT * FROM users WHERE id = $(id)", { id: id.id }).then(
+//         (todo) => res.json(todo)
+//       );
+//     })
+//     .catch((error) => res.status(500).send(error));
+// });
 
 userRoutes.post("/signup", (req, res) => {
-  const newUser = {
+  bcrypt.genSalt(saltRounds, function (err, salt) {
+    bcrypt.hash(req.body.password, salt, function (err, hash) {
+      const newUser = {
+        username: req.body.username,
+        password: hash,
+        first_name: req.body.first_name,
+        last_name: req.body.last_name,
+        email: req.body.email,
+        birthdate: req.body.birthdate,
+        city: req.body.city,
+        state: req.body.state,
+        country: req.body.country,
+        zip: req.body.zip,
+      };
+      db.one(
+        "INSERT INTO users ( username, password, first_name, last_name, email, birthdate, city, state, country, zip ) VALUES \
+            ( ${username}, ${password}, ${first_name}, ${last_name}, ${email}, ${birthdate}, ${city}, ${state}, ${country}, ${zip} ) RETURNING id;",
+        newUser
+      )
+        .then((id) => {
+          return db.oneOrNone("SELECT * FROM users WHERE id=${id}", {
+            id: id.id,
+          });
+        })
+        .then((user) => res.json(user));
+    });
+  });
+});
+
+userRoutes.post("/login", (req, res) => {
+  const userLoginInput = {
     username: req.body.username,
     password: req.body.password,
-    first_name: req.body.first_name,
-    last_name: req.body.last_name,
-    email: req.body.email,
-    birthdate: req.body.birthdate,
-    city: req.body.city,
-    state: req.body.state,
-    country: req.body.country,
-    zip: req.body.zip,
   };
 
-  db.one(
-    "INSERT INTO users ( username, password, first_name, last_name, email, birthdate, city, state, country, zip ) VALUES \
-    ( ${username}, ${password}, ${first_name}, ${last_name}, ${email}, ${birthdate}, ${city}, ${state}, ${country}, ${zip} ) returning id;",
-    newUser
-  )
-    .then((id) => {
-      return db.oneOrNone("SELECT * FROM users WHERE id=${id}", { id: id.id });
-    })
-    .then((user) => res.json(user));
+  db.oneOrNone(
+    "SELECT id, username, password FROM users WHERE username = $(username)",
+    { username: req.body.username }
+  ).then((userCredentials) => {
+    if (!userCredentials) {
+      return res.status(400).send("Invalid email or password");
+    }
+
+    if (
+      !bcrypt.compareSync(userLoginInput.password, userCredentials.password)
+    ) {
+      return res.status(401).send("Invalid Username or Password");
+    }
+
+    res.status(200).json(userCredentials);
+  });
 });
 
 export default userRoutes;
