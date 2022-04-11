e5614eda418e3e26e39981370703e3266c2e641e
a-brown77 <a.brown7795@gmail.com>
2022-04-11 17:49:23 -0400

    header stuff, maybe broken

--- smash-community-app/src/App.tsx
+++ smash-community-app/src/App.tsx
@@ -1,9 +1,17 @@
 import React from "react";
 import logo from "./logo.svg";
+import { useLocation } from "react-router-dom";
 import "./App.css";
+import {Header} from "../src/components/Header"
 
 function App() {
-  return <div className="App"></div>;
+  const location =useLocation();
+
+  if(location.pathname === '/' || location.pathname === 'login' || location.pathname === 'signup'){ 
+    return null 
+  } else { 
+    return <Header></Header>
+  }
 }
 
 export default App;
