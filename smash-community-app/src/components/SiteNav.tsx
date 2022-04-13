ae7f02dce09045cf3295b61ba2e5304c2e8f707a
CD Rivers <riverscd90@gmail.com>
2022-04-12 20:42:13 -0400

    mui styling on sitenav

--- smash-community-app/src/components/SiteNav.tsx
+++ smash-community-app/src/components/SiteNav.tsx
@@ -1,33 +1,139 @@
 import { Link } from "react-router-dom";
 
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import MuiLink from '@mui/material/Link';
+import Paper from "@mui/material/Paper";
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+// import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider, useTheme } from '@mui/material/styles';
+
+
+const darkTheme = createTheme({
+  palette: {
+    mode: "dark",
+  },
+});
+
 export function SiteNavigation() {
+
+
   return (
-    <div>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/mycommunities">My Communities</Link>
-          </li> 
-         <li>
-            <Link to="/communityfinder">Community Finder</Link>
-          </li> 
-           <li>
-            <Link to="/myevents">My Events</Link>
-          </li>
-          <li>
-            <Link to="/myprofile">My Profile</Link>
-          </li>
-          <li>
-            <Link to="/eventfinder">Event Finder</Link>
-          </li>
-          <li>
-            <Link to="/communityeventmanager">Community/Event Manager</Link>
-          </li>
-          <li>
-            <Link to="/login">Sign Out</Link>
-          </li>
-        </ul>
-      </nav>
-    </div>
+    <ThemeProvider theme={darkTheme}>
+      <Container component="main" >
+        <CssBaseline />
+        <Grid
+          item
+          xs={12}
+          // sm={8}
+          component={Paper}
+          elevation={6}
+          sx={{
+            py: 2
+          }}
+        >
+          <Box sx={{
+            mx: 4,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'left',
+          }}>
+            <Typography component="h1" variant="h5">
+              Explore
+            </Typography>
+            <Grid container >
+              <Grid item>
+                <MuiLink href="/communityfinder" variant="inherit" color="inherit" underline="none">
+                  Community Finder
+                </MuiLink>
+              </Grid>
+            </Grid>
+            <Grid container>
+              <Grid item>
+                <MuiLink href="/eventfinder" variant="inherit" color="inherit" underline="none">
+                  Event Finder
+                </MuiLink>
+              </Grid>
+            </Grid>
+          </Box>
+          <Box sx={{
+            mx: 4,
+            mt: 4,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'left',
+          }}>
+            <Typography component="h1" variant="h5">
+              My Stuff
+            </Typography>
+            <Grid container >
+              <Grid item>
+                <MuiLink href="/myevents" variant="inherit" color="inherit" underline="none">
+                  My Events
+                </MuiLink>
+              </Grid>
+            </Grid>
+            <Grid container >
+              <Grid item>
+                <MuiLink href="/mycommunities" variant="inherit" color="inherit" underline="none">
+                  My Communities
+                </MuiLink>
+              </Grid>
+            </Grid>
+            <Grid container >
+              <Grid item>
+                <MuiLink href="/myprofile" variant="inherit" color="inherit" underline="none">
+                  My Profile
+                </MuiLink>
+              </Grid>
+            </Grid>
+            <Grid container>
+              <Grid item>
+                <MuiLink href="/login" variant="inherit" color="inherit" underline="none">
+                  Sign Out
+                </MuiLink>
+              </Grid>
+            </Grid>
+          </Box>
+          {/* <Copyright sx={{ mt: 5 }} /> */}
+        </Grid>
+      </Container>
+    </ThemeProvider>
+
+
+    // <div>
+    //   <nav>
+    //     <ul>
+    //       <li>
+    //         <Link to="/mycommunities">My Communities</Link>
+    //       </li> 
+    //      <li>
+    //         <Link to="/communityfinder">Community Finder</Link>
+    //       </li> 
+    //        <li>
+    //         <Link to="/myevents">My Events</Link>
+    //       </li>
+    //       <li>
+    //         <Link to="/myprofile">My Profile</Link>
+    //       </li>
+    //       <li>
+    //         <Link to="/eventfinder">Event Finder</Link>
+    //       </li>
+    //       <li>
+    //         <Link to="/communityeventmanager">Community/Event Manager</Link>
+    //       </li>
+    //       <li>
+    //         <Link to="/login">Sign Out</Link>
+    //       </li>
+    //     </ul>
+    //   </nav>
+    // </div>
   );
 }
