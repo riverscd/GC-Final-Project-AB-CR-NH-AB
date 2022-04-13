1c60b683e57f275b28331fb8af6d4c0f9f5afbd8
a-brown77 <a.brown7795@gmail.com>
2022-04-12 20:38:09 -0400

    oops, more

--- smash-community-app/src/components/SiteNav.tsx
+++ smash-community-app/src/components/SiteNav.tsx
@@ -1,6 +1,17 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import UserContext from "../contexts/UserContext";
+import {useContext} from "react";
+import { User } from "../models/users";
 
 export function SiteNavigation() {
+  const { loggedInUser, removeUser } = useContext(UserContext); 
+  const navigate = useNavigate();
+
+  function handleSignOut() { 
+    removeUser(loggedInUser as User)
+    navigate("/login")
+  }
+
   return (
     <div>
       <nav>
@@ -23,10 +34,8 @@ export function SiteNavigation() {
           <li>
             <Link to="/communityeventmanager">Community/Event Manager</Link>
           </li>
-          <li>
-            <Link to="/login">Sign Out</Link>
-          </li>
         </ul>
+        <button type="submit" onClick={handleSignOut}>Sign Out</button>
       </nav>
     </div>
   );
