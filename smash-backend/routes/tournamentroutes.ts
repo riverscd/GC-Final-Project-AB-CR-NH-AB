3a187d2fc914663b77f753f67d1e838a51955077
CD Rivers <riverscd90@gmail.com>
2022-04-14 19:37:58 -0400

    sorted filter route for by state hardcoded to MI

--- smash-backend/routes/tournamentroutes.ts
+++ smash-backend/routes/tournamentroutes.ts
@@ -66,67 +66,62 @@ tournamentroutes.get("/tournaments", (req: any, res: any) => {
     .then((data) => res.status(200).json(data));
 });
 
-// tournamentroutes.get(`/byState`, (req: any, res: any) => {
-//   const perPage: number = 5;
-//   const videogameId: number = 1;
-//   // const past : boolean = false;
-//   const token : string = process.env.SMASH_AUTH_TOKEN!;
-//   const state: string = req.params.state;//this might be be broken
-//   const query : string = `
-//   query TournamentsByVideogameAndState($perPage: Int!, $videogameId: ID!, $state: String!) {
-//     tournaments(query: {
-//       perPage: $perPage
-//       page: 1
-//       sortBy: "startAt asc"
-//       filter: {
-//         addrState: $state,
-//         videogameIds: [$videogameId]
-//       }
-//     }){
-//       nodes {
-//         id
-//         name
-//               city
-//               state
-//               postalCode
-//               venueName
-//               countryCode
-//               endAt
-//               events{competitionTier
-//                 name}
-//               hasOfflineEvents
-//               hasOnlineEvents
-//               images{url}
-//               isRegistrationOpen
-//               name
-//               numAttendees
-//               registrationClosesAt
-//               slug
-//               startAt
-//       }
-//     }
-//   }
-//   `;
+tournamentroutes.get(`/tournaments/bystate`, (req: any, res: any) => {
+  const perPage: number = 5;
+  const videogameId: number = 1;
+  // const past : boolean = false;
+  const token : string = process.env.SMASH_AUTH_TOKEN!;
+  const addrState: string = "MI" ;
+  const query : string = `
+  query TournamentsByVideogameAndState($perPage: Int!, $videogameId: ID!, $addrState: String!) {
+    tournaments(query: {
+      perPage: $perPage
+      page: 1
+      filter: {
+        videogameIds: [$videogameId]
+        addrState: $addrState,
+      }
+    }){
+      nodes {
+        id
+        name
+              city
+              addrState
+              postalCode
+              venueName
+              countryCode
+              endAt
+              events{competitionTier
+                name}
+              hasOfflineEvents
+              hasOnlineEvents
+              images{url}
+              isRegistrationOpen
+              name
+              numAttendees
+              registrationClosesAt
+              slug
+              startAt
+      }
+    }
+  }
+  `;
 
-//   fetch("https://api.smash.gg/gql/alpha", {
-//     method: "POST",
-//     headers: {
-//       "Content-Type": "application/json",
-//       Accept: "application/json",
-//       Authorization: `Bearer ${token}`,
-//     },
-//     body: JSON.stringify({
-//       query,
-//       variables: { 
-//         perPage, 
-//         videogameId, 
-//         state 
-//       },
-//     }),
-//   })
-//   .then((r) => r.json())
-//   .then(data => console.log('data returned:', data))
-//   // .then((data) => res.status(200).json(data));
-// })
+  fetch("https://api.smash.gg/gql/alpha", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+    body: JSON.stringify({
+      query,
+      variables: { perPage, videogameId, addrState },
+    }),
+  })
+  .then((r) => r.json())
+  // .then(data => console.log('data returned:', data))
+ .then((data) => res.status(200).json(data));
+})
 
 export default tournamentroutes;
