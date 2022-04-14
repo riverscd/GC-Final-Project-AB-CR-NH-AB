75669e01efef6bde9073cc64d6a3dbebde054341
CD Rivers <riverscd90@gmail.com>
2022-04-13 21:18:52 -0400

    updated basic model for message board and message with routes

--- smash-community-app/src/components/GeneralMessageBoard.tsx
+++ smash-community-app/src/components/GeneralMessageBoard.tsx
@@ -15,81 +15,44 @@ export function GeneralMessageBoard() {
             <button>Create Post</button>
 
             <div className="message">
-                <p> Message Title: </p>
+            <Link to ="/message" > Message Title: </Link>
                 <p> Username of Author: </p>
-                <p> Date Posted </p>
+                <p> Date Posted: </p>
                 <p> Message Body: PUT THIS ON MESSAGE PAGE NOT MAIN BOARD </p>
-                <p>Show Replies on Main Board like below
-                    or show reply count and click Message to see Replies?</p>
-                <button>reply</button> <button  onClick= {handleClick} >Replies</button>  <button>way to report?</button>
+                <p># of replies </p>
+                <button onClick= {handleClick}>reply</button>  
             </div>
 
-            <div className="reply">
-                <p> Username of Reply Author: </p>
-                <p> Date Posted </p>
-                <p> Reply Body: </p>
-                <p>if allow replies to replies, indicate re: to somehow?</p>
-                <button>reply?</button> <button>way for creator to delete</button> <button>way to report?</button>
+
+            <div className="message">
+            <Link to ="/message" > Message Title: </Link>
+                <p> Username of Author: </p>
+                <p> Date Posted: </p>
+                <p> Message Body: </p>
+                <p># of replies </p>
+                <button>reply</button> 
             </div>
 
-            <div className="reply">
-            <p> Username of Reply Author: </p>
-                <p> Date Posted </p>
-                <p> Reply Body: </p>
-                <button>reply?</button>  <button>way to report?</button>
+         
+            <div className="message">
+            <Link to ="/message" > Message Title: </Link>
+                <p> Username of Author: </p>
+                <p> Date Posted: </p>
+                <p> Message Body: </p>
+                <p># of replies </p>
+                <button>reply</button> 
             </div>
 
             <div className="message">
-                <p> Message Title: </p>
+            <Link to ="/message" > Message Title: </Link>
                 <p> Username of Author: </p>
-                <p> Date Posted </p>
+                <p> Date Posted: </p>
                 <p> Message Body: </p>
-                <button>reply</button> <button>way to report?</button>
+                <p># of replies </p>
+                <button>reply</button>
             </div>
 
-            <div className="reply">
-            <p> Username of Reply Author: </p>
-                <p> Date Posted </p>
-                <p> Reply Body: </p>
-                <button>reply?</button> <button>way to report?</button>
-            </div>
-
-            <div className="message">
-                <p> Message Title: </p>
-                <p> Username of Author: </p>
-                <p> Date Posted </p>
-                <p> Message Body: </p>
-                <button>reply</button> <button>way to report?</button>
-            </div>
-
-            <div className="message">
-                <p> Message Title: </p>
-                <p> Username of Author: </p>
-                <p> Date Posted </p>
-                <p> Message Body: </p>
-                <button>reply</button> <button>way to report?</button>
-            </div>
-
-            <div className="reply">
-            <p> Username of Reply Author: </p>
-                <p> Date Posted </p>
-                <p> Reply Body: </p>
-                <button>reply?</button> <button>way to report?</button>
-            </div>
-
-            <div className="reply">
-            <p> Username of Reply Author: </p>
-                <p> Date Posted </p>
-                <p> Reply Body: </p>
-                <button>reply?</button> <button>way to report?</button>
-            </div>
-
-            <div className="reply">
-            <p> Username of Reply Author: </p>
-                <p> Date Posted </p>
-                <p> Reply Body: </p>
-                <button>reply?</button> <button>way to report?</button>
-            </div>
+           
 
 
 
--- smash-community-app/src/components/Message.tsx
+++ smash-community-app/src/components/Message.tsx
@@ -1,12 +1,47 @@
+import { Link } from "react-router-dom";
+
 export function Message(){
 
 
     return (
         <div>
-           Message Title: 
-           Author:
-           Date Posted:
-           Message Body: 
+            <Link to="/generalmessageboard">Back to Message Board</Link>
+          <div className="message">
+                <p> Message Title: </p>
+                <p> Username of Author: </p>
+                <p> Date Posted: </p>
+                <p> Message Body: </p>
+                <button>reply</button> 
+            </div>
+
+            <div className="reply">
+                <p> Username of Reply Author: </p>
+                <p> Date Posted: </p>
+                <p> Reply Body: </p>
+                
+                 
+            </div>
+
+            <div className="reply">
+            <p> Username of Reply Author: </p>
+                <p> Date Posted: </p>
+                <p> Reply Body: </p>
+                
+            </div>
+            <div className="reply">
+            <p> Username of Reply Author: </p>
+                <p> Date Posted: </p>
+                <p> Reply Body: </p>
+                  
+            </div>
+
+
+           
+          
+         
+          
+
+
         </div>
     )
 }
