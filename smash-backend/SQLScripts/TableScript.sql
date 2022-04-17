a931dd55ba0140d3bcf8fe1e3d14cb44e01d565f
Nick <nhamer233@gmail.com>
2022-04-17 05:12:09 -0400

    v close on edit profile added sql file with script, npm i for react-select

---
+++ smash-backend/SQLScripts/TableScript.sql
@@ -0,0 +1,126 @@
+drop table communities;
+drop table events;
+drop table replies;
+drop table posts;
+drop table users;
+drop table characters;
+
+create table characters(
+id serial PRIMARY KEY,
+character_name text NOT NULL
+);
+
+create table users(
+id serial PRIMARY KEY,
+username varchar(15) UNIQUE CHECK(LENGTH(username)>=3),
+password char(60) NOT NULL,
+first_name varchar(50) CHECK(LENGTH(first_name)>=1),
+last_name varchar(50) CHECK(LENGTH(last_name)>=1),
+email varchar(50) UNIQUE NOT NULL,
+birthdate date,
+city varchar(50),
+state varchar(2),
+country varchar(50),
+zip varchar(5),
+user_profile_img bytea,
+bio text,
+main_character smallint[],
+secondary_characters smallint[],
+slippi_usernames varchar(20)[]
+);
+
+create table posts(
+id serial PRIMARY KEY,
+author_id int REFERENCES users ON DELETE CASCADE,
+post_title text NOT NULL,
+post_message text NOT NULL,
+replies int[],
+date_created timestamp 
+);
+
+create table replies(
+id serial PRIMARY KEY,
+author_id int REFERENCES users ON DELETE CASCADE,
+post_id int REFERENCES posts ON DELETE CASCADE,
+message text NOT NULL,
+date_created timestamp NOT NULL
+);
+
+create table events(
+id serial PRIMARY KEY,
+creator_id int REFERENCES users ON DELETE CASCADE,
+event_name varchar(50) NOT NULL,
+attendees int[],
+description text,
+event_date timestamp NOT NULL,
+posts int[],
+event_img bytea,
+is_in_person boolean,
+location text,
+address text,
+city varchar(30),
+state varchar(2),
+country varchar(50),
+zip varchar(5)
+);
+
+create table communities(
+id serial PRIMARY KEY,
+community_name varchar(50) UNIQUE NOT NULL,
+creator_id int,
+community_members_id int[],
+location text,
+posts int[],
+community_profile_img bytea,
+description text
+);
+
+INSERT INTO characters (character_name) values
+('Mario'), 
+('Luigi'), 
+('Yoshi'), 
+('Donkey Kong'), 
+('Link'), 
+('Samus'), 
+('Kirby'), 
+('Fox'), 
+('Pikachu'), 
+('Jigglypuff'), 
+('Cpt. Falcon'), 
+('Ness'), 
+('Peach'), 
+('Bowser'), 
+('Dr. Mario'), 
+('Zelda'), 
+('Sheik'), 
+('Ganondorf'), 
+('Young Link'), 
+('Falco'), 
+('Mewtwo'), 
+('Pichu'), 
+('Ice Climbers'), 
+('Marth'), 
+('Roy'), 
+('Mr. Game & Watch');
+
+insert into users (username, password, first_name, last_name, email, birthdate, city, state, country, zip, bio, main_character, secondary_characters, slippi_usernames) 
+values ('Hamer','pass','Nick','Hamer','nhamer@gmail.com','1991-08-26', 'Lansing', 'MI', 'United States of American', '55555', 'Avid gamer with a passion for programming and learning new stuff', '{23}', '{11}', '{nhamer}');
+insert into users (username, password, first_name, last_name, email, birthdate, city, state, country, zip, bio, main_character, secondary_characters, slippi_usernames) 
+values ('alifi','pass','Adam','Boeving','adamboeving@gmail.com','1993-07-02', 'Holt', 'MI', 'United States of American', '48842', 'Avid gamer with a passion for programming and learning new stuff',  '{8}', '{6}', '{aalifi}');
+
+insert into communities (community_name, creator_id, community_members_id, location, posts, description) 
+values ('Lansing Gamers', 1, '{1}', 'Lansing','{1}','Local to Lansing, looking to play pick up games');
+insert into communities (community_name, creator_id, community_members_id, location, posts, description) 
+values ('Holt Gamers', 2, '{2}', 'Holt','{2}','Local to Holt, looking to play pick up games');
+
+insert into events (creator_id, event_name, attendees, description, event_date, posts,is_in_person, location, address, city, state, country, zip) 
+values (1, 'Smashfest Holt', '{10}', 'Smash Tournament located in Holt', '2022-09-03', '{0}', true, 'Holt', '5555555 cedar st', 'Holt', 'MI', 'United States Of America', '48842');
+insert into events (creator_id, event_name, attendees, description, event_date, posts,is_in_person, location, address, city, state, country, zip) 
+values (1, 'Smashfest Lansing', '{8}', 'Smash Tournament located in Lansing', '2022-09-03', '{0}', true, 'Lansing', '5555555 Capital Ave', 'Lansing', 'MI', 'United States Of America', '48864');
+
+INSERT INTO posts (author_id, post_title, post_message, date_created) values ('1', 'First!', 'first! lol', '2022-04-13');
+INSERT INTO posts (author_id, post_title, post_message, date_created) values ('2', 'Not First', 'not first :(', '2022-04-13');
+
+INSERT INTO replies (author_id, post_id, message, date_created) values ('1', '1', 'testing reply post 1', '2022-04-16');
+INSERT INTO replies (author_id, post_id, message, date_created) values ('2', '2', 'testing reply post 2', '2022-04-16');
+
--- smash-community-app/src/components/EditProfile.tsx
+++ smash-community-app/src/components/EditProfile.tsx
@@ -1,59 +1,99 @@
-import { useContext, useEffect, useState } from "react";
+import { KeyboardEventHandler, useContext, useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import UserContext from "../contexts/UserContext";
-import { Character } from "../models/characters";
-import { GetAllCharacters } from "../services/characters";
+import { characters } from "../models/characters";
 import { UpdateUser } from "../services/users";
+import Select from "react-select";
+
+import CreatableSelect from "react-select";
+import { ActionMeta, OnChangeValue, MultiValue } from "react-select";
 
 export function EditProfile() {
+  const components = {
+    DropdownIndicator: null,
+  };
+
+  interface Option {
+    readonly label: string;
+    readonly value: string;
+  }
+
+  const createOption = (label: string) => ({
+    label,
+    value: label,
+  });
+
+  interface State {
+    readonly inputValue: string;
+    readonly value: readonly Option[];
+  }
+
   const navigate = useNavigate();
+  const { loggedInUser, addUser } = useContext(UserContext);
+
   const [firstName, setFirstName] = useState("");
   const [lastName, setLastName] = useState("");
   const [city, setCity] = useState("");
-  const [state, setState] = useState("");
+  const [idk, setidk] = useState("");
+  const [state, setState] = useState<OnChangeValue<Option, true>>([]);
+  const [input, setInput] = useState<string>("");
   const [country, setCountry] = useState("");
   const [bio, setBio] = useState("");
-  const [slippi, setSlippi] = useState([]);
+  const [slippi, setSlippi] = useState<string[]>([]);
+  const [mainCharacters, setMainCharacters] = useState<number[]>([]);
+  const [secondaryCharacters, setSecondaryCharacters] = useState<number[]>([]);
 
-  const [characters, setCharacters] = useState<Character[]>([]);
-  const [mainCharacters, setMainCharacter] = useState<number>(0);
-  const [secondaryCharacters, setSecondaryCharacters] = useState([]);
-  const { loggedInUser, addUser } = useContext(UserContext);
-  console.log(loggedInUser?.id);
-  useEffect(() => {
-    GetAllCharacters().then((data: any) => {
-      setCharacters(data);
-    });
-    // GetUser(id).then((data: any) => {
-    //   setUsers(data);
-    // });
-  }, []);
-
+  const options = characters.map((character) => {
+    return { value: character.id, label: character.character_name };
+  });
+  console.log(mainCharacters);
   function handleSubmit(e: any) {
     e.preventDefault();
     console.log(loggedInUser?.id);
-    UpdateUser(
-      loggedInUser?.id,
-      firstName,
-      lastName,
-      city,
-      state,
-      country,
-      bio
-      //mainCharacter
-      // secondaryCharacters[],
-    ).then((updatedUser) => {
-      if (updatedUser) {
-        addUser(updatedUser);
-        navigate("/myprofile");
-      }
-    });
+
+    // UpdateUser(
+    //   loggedInUser?.id,
+    //   firstName,
+    //   lastName,
+    //   city,
+    //   idk,
+    //   country,
+    //   bio,
+    //   mainCharacters,
+    //   secondaryCharacters
+    // ).then((updatedUser) => {
+    //   if (updatedUser) {
+    //     addUser(updatedUser);
+    //     navigate("/myprofile");
+    //   }
+    // });
   }
 
+  const handleChange = (value: OnChangeValue<Option, true>) => {
+    setState(value);
+  };
+  const handleInputChange = (inputValue: string) => {
+    setInput(inputValue);
+  };
+  const handleKeyDown: KeyboardEventHandler<HTMLDivElement> = (event) => {
+    if (!input) return;
+    switch (event.key) {
+      case "Enter":
+      case "Tab":
+        console.group("Value Added");
+
+        console.groupEnd();
+        setState([...state, createOption(input)]);
+        setInput("");
+        setSlippi(state.map((option) => option.value));
+        console.log(slippi);
+        event.preventDefault();
+    }
+  };
   return (
     <div>
       <form>
-        <p>Create Your Profile</p>
+        <p>Edit Your Profile</p>
         <label>
           <p>First Name:</p>
           <input type="text" onChange={(e) => setFirstName(e.target.value)} />
@@ -62,16 +102,12 @@ export function EditProfile() {
           <p>Last Name:</p>
           <input type="text" onChange={(e) => setLastName(e.target.value)} />
         </label>
-        {/* <label>
-            <p>Slippi Username:</p>
-            <input type="text" onChange={(e) => setSlippi(e.target.value)} />
-          </label> */}
         <label>
           <p>Location Information:</p>
           <p>Country:</p>
           <input type="text" onChange={(e) => setCountry(e.target.value)} />
           <p>State:</p>
-          <input type="text" onChange={(e) => setState(e.target.value)} />
+          {/* <input type="text" onChange={(e) => setState(e.target.value)} /> */}
           <p>City:</p>
           <input type="text" onChange={(e) => setCity(e.target.value)} />
         </label>
@@ -79,100 +115,51 @@ export function EditProfile() {
           <p>User Bio:</p>
           <input type="text" onChange={(e) => setBio(e.target.value)} />
         </label>
+        <br />
+        <label>
+          Mains:
+          <Select
+            options={options}
+            isMulti
+            name="main_characters"
+            className="basic-multi-select"
+            classNamePrefix="select"
+            onChange={(newValues) => {
+              setMainCharacters(newValues.map((newValue) => newValue.value));
+            }}
+          />
+        </label>
+        <label>
+          Secondaries:
+          <Select
+            options={options}
+            isMulti
+            name="secondary_characters"
+            className="basic-multi-select"
+            classNamePrefix="select"
+            onChange={(newValues) => {
+              setSecondaryCharacters(
+                newValues.map((newValue) => newValue.value)
+              );
+            }}
+          />
+        </label>
+        <CreatableSelect
+          components={components}
+          inputValue={input}
+          isClearable
+          isMulti
+          menuIsOpen={false}
+          onChange={handleChange}
+          onInputChange={handleInputChange}
+          onKeyDown={handleKeyDown}
+          placeholder="Type something and press enter..."
+          value={state}
+        />
         <button className="button" type="submit" onClick={handleSubmit}>
           Submit
         </button>
       </form>
-      <div>
-        {/* <h2>Choose Your Character</h2>
-        Main Character - {mainCharacter}
-        Secondary Character- {secondaryCharacters} */}
-        {/* <div>
-          <label>Main Character: </label>
-
-          <select
-            onChange={(event) => {
-              setMainCharacter(parseInt(event.target.value));
-            }}
-          >
-            {characters?.map((character: Character) => (
-              <option
-                value={`${character.id}`}
-              >{`${character.character_name}`}</option>
-            ))}
-          </select>
-        </div> */}
-        <div>
-          {/* {/* <label>Secondary Character 1: </label> */}
-
-          {/* onChange={(event) => {
-              setSecondaryCharacters(event.target.value);
-            }} */}
-          {/* <select> 
-            {characters?.map((character: Character) => (
-              <option
-                value={`${character.character_name}`}
-              >{`${character.character_name}`}</option>
-            ))}
-          </select>
-        </div>
-        <div>
-          <label>Secondary Character 2: </label>
-          <select
-            onChange={(event) => {
-              setSecondaryCharacter2(event.target.value);
-            }}
-          >
-            {characters?.map((character: Character) => (
-              <option
-                value={`${character.character_name}`}
-              >{`${character.character_name}`}</option>
-            ))}
-          </select>
-        </div>
-        <div>
-          <label>Secondary Character 3: </label>
-          <select
-            onChange={(event) => {
-              setSecondaryCharacter3(event.target.value);
-            }}
-          >
-            {characters?.map((character: Character) => (
-              <option
-                value={`${character.character_name}`}
-              >{`${character.character_name}`}</option>
-            ))}
-          </select>
-        </div>
-        <div>
-          <label>Secondary Character 4: </label>
-          <select
-            onChange={(event) => {
-              setSecondaryCharacter4(event.target.value);
-            }}
-          >
-            {characters?.map((character: Character) => (
-              <option
-                value={`${character.character_name}`}
-              >{`${character.character_name}`}</option>
-            ))}
-          </select> */}
-          {/* </div>
-        <div>
-          <label>Secondary Character 5: </label>
-          <select
-            onChange={(event) => {
-              setSecondaryCharacter5(event.target.value);
-            }}
-          >
-            {characters?.map((character: Character) => (
-              <option
-                value={`${character.character_name}`}
-              >{`${character.character_name}`}</option>
-            ))}
-          </select> */}
-        </div>
-      </div>
     </div>
   );
 }
