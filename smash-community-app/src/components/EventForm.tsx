7b5a6b0956e39c5fb0541bb389d56ec24658a104
CD Rivers <riverscd90@gmail.com>
2022-04-19 01:52:53 -0400

    basic formik/yup/mui integration for communityform. design tbd

--- smash-community-app/src/components/EventForm.tsx
+++ smash-community-app/src/components/EventForm.tsx
@@ -124,7 +124,7 @@ export function CreateEvent() {
             ).then((newEvent) => {
               if (newEvent) {
                 //  addUser(user);
-                navigate("/");
+                navigate("/communityeventmanager");
               }
             });
           }}>
